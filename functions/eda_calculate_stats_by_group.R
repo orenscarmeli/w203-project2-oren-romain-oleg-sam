5722186dac41e486df9bf20b05581dd836b909b2
orenscarmeli <oren.carmeli95@gmail.com>
2021-12-06 23:35:51 -0800

    add the categorical EDA

--- functions/eda_calculate_stats_by_group.R
+++ functions/eda_calculate_stats_by_group.R
@@ -1,22 +1,25 @@
 eda_calculate_stats_by_group <-
-  function(column, quantile_table = FALSE) {
-    
+  function(column, dt, quantile_table = FALSE) {
     # calc summary statistics per unique element in categorical column
-    result <- d_dt[, .(count_apps = .N,
-                       install_group_avg = mean(install_group, na.rm = TRUE),
-                       install_count_med = median(installs, na.rm = TRUE)), 
-                   by = column][count_apps > 100][order(-install_group_avg)]
-    setnames(result, column, 'group_by_column')
-    result[, variable := column]
+    result <- dt[, .(
+      `Count Apps` = .N,
+      `Log of Install Count (Avg)` = mean(log_installs, na.rm = TRUE)
+    ),
+    by = column][`Count Apps` > 100][order(-`Log of Install Count (Avg)`)]
+    setnames(result, column, 'Label')
+    result[, `Variable` := column]
     
     # create a table that shows the distribution across group averages
     if (quantile_table) {
-      result <- as.data.table(t(data.table(quantile(
-        result$install_group_avg
-      ))))
+      result <- as.data.table(t(data.table(
+        quantile(result$`Log of Install Count (Avg)`)
+      )))
       setnames(result, c('0%', '25%', '50%', '75%', '100%'))
-      result[, variable := column]
-      result[, diff_min_vs_max := `100%` - `0%`]
+      result[, `Variable` := column]
+      result[, `Diff (Max - Min)` := `100%` - `0%`]
+      result[, `Diff (Max - Min) / Min` := `Diff (Max - Min)` / `0%`]
+      result <- subset(result, select = c('Variable','0%', '25%', '50%', '75%', '100%','Diff (Max - Min)','Diff (Max - Min) / Min'))
+      setorder(result,`Diff (Max - Min)`)
     }
     return(result)
   }
